380695274185a46b17f7774c9e294eede0ae3711
FraFrieFa <fabius2001@hotmail.de>
2020-10-03 18:26:44 +0200

    Test Classes

--- src/de/spd_bautzen/Element.java
+++ src/de/spd_bautzen/Element.java
@@ -9,14 +9,22 @@ public class Element {
 	private Map<String, String> attributes;
 	
 	public Element(ElementType type) {
+		if(type == null)
+			throw new NullPointerException();
+		this.type = type;
 		attributes = new HashMap<String, String>();
 	}
 	
 	public String getAttribute(String key) {
+		if(key == null || key.isEmpty())
+			throw new NullPointerException();
 		return attributes.get(key);
 	}
 	
-	
+	public Map<String, String> getAttributes() {
+		return attributes;
+	}
+
 	public void setAttribute(String key, String value) {
 		attributes.put(key, value);
 	}
--- src/de/spd_bautzen/test/TestElement.java
+++ src/de/spd_bautzen/test/TestElement.java
@@ -2,18 +2,58 @@ package de.spd_bautzen.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import de.spd_bautzen.Element;
+import de.spd_bautzen.ElementType;
 
 class TestElement {
 
-	@AfterEach
-	void tearDown() throws Exception {
+	@Test
+	public void ELEMENT_element_null() {
+		boolean thrown = false;
+		try {
+			@SuppressWarnings("unused")
+			Element e = new Element(null);
+		} catch (Exception ex) {
+			if (ex.getClass() == NullPointerException.class) {
+				thrown = true;
+			}
+		}
+		assertTrue(thrown, "null argument in constructor did not throw nullpointer exception");
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void ELEMENT_inv_test() {
+		Element e = new Element(ElementType.IMAGE);
+		assertTrue(e.type != null, "elements type is null");
+		assertTrue(e.getAttributes() != null, "elements attributes are null");
+	}
+
+	@Test
+	public void ELEMENT_getAttributeValue() {
+		Element e = new Element(ElementType.IMAGE);
+		e.setAttribute("wasser", "nass");
+		boolean thrown = false;
+		try {
+			e.getAttribute(null);
+		}catch(Exception ex) {
+			if(ex.getClass() == NullPointerException.class) {
+				thrown = true;
+			}
+		}
+		assertTrue(thrown, "null argument in getAttributeValue did not throw nullpointer exception");
+		try {
+			e.getAttribute("");
+		}catch(Exception ex) {
+			if(ex.getClass() == NullPointerException.class) {
+				thrown = true;
+			}
+		}
+		assertTrue(thrown, "empty string in getAttributeValue did not throw nullpointer exception");
+		assertTrue(e.getAttribute("was") == null, "wrong return value when element does not have attribute");
+		assertTrue(e.getAttribute("wasser").equals("nass"), "wrong return value when element does have attribute");
+		
 	}
 
 }
